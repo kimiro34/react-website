45749996de07ead5c2bba53869940dfda6c13d41
Kimiro <89557468+kimiro34@users.noreply.github.com>
2022-07-23 11:58:04 +0100

    Update index.tsx

--- src/Constant/index.tsx
+++ src/Constant/index.tsx
@@ -1,4 +1,4 @@
 export const client_id =
-  "233345635594-km0tlqqrv2difnjgovf2jn11sgg7117c.apps.googleusercontent.com";
+  "355684352609-b1h4fo5qsu8m3tb2r2k4u16vc00d131p.apps.googleusercontent.com";
 export const REQUEST_API_URL =
   "https://62dbb7ac4438813a2608fbad.mockapi.io/api/getnames/names";
